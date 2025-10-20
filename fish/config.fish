eee9d98cbc9f1ff73881dd2bb64d5237997c801f
citrus <citrus.mikan88@gmail.com>
2025-10-20 22:53:30 +0900

    add  dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # PATH
+    set PATH /opt/homebrew/bin $PATH # homebrew
+    set PATH ~/.cargo/bin $PATH # cargo
+end
+starship init fish | source
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/citrus/.lmstudio/bin
+# End of LM Studio CLI section
+
